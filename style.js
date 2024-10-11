175cc5ab0c0aaf4f244267ebc1f3a1b616499803
kaiki <kanokaiki@gmail.com>
2024-10-10 19:50:19 -0700

    feat: Implementation of the sticky element

---
+++ style.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    const button = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
